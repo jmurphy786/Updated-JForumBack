5d074ec199bbf163d6579cd271ce826bc1348a28
Rajan <rajan96@hotmail.co.uk>
2018-12-06 11:57:33 +0000

    Another Update

--- src/main/java/com/qa/SweetSpot/controller/SpringBootController.java
+++ src/main/java/com/qa/SweetSpot/controller/SpringBootController.java
@@ -7,20 +7,25 @@ import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
+import java.util.Optional;
 
 import javax.imageio.ImageIO;
 import javax.validation.Valid;
 
-
+import org.apache.commons.io.FilenameUtils;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,7 +39,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.qa.SweetSpot.exception.ResourceNotFoundException;
-import com.qa.SweetSpot.mySpringBootDatabaseApp.Users;
 import com.qa.SweetSpot.repository.*;
 import com.qa.SweetSpot.mySpringBootDatabaseApp.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -47,8 +51,7 @@ public class SpringBootController {
 
 	
 	private static final Logger logger = LoggerFactory.getLogger(SpringBootController.class);
-	
-	
+	private String extension = "";
 	private static final ResponseEntity<?> ResponseEntitiy = null;
 
 	@Autowired
@@ -62,8 +65,8 @@ public class SpringBootController {
 	
 
 	
-	public void saveFileToDb(MultipartFile multipart, Long postId) throws IOException {
-	    File convFile = new File("C:\\Users\\Admin\\Documents\\JForum\\Forum-back-master\\images\\" + postId + ".jpg");
+	public void saveFileToDb(MultipartFile multipart, Long postId, String extension) throws IOException {
+	    File convFile = new File("C:\\Users\\Admin\\Documents\\JForum\\Forum-back-master\\images\\" + postId + "." + extension);
 	    FileOutputStream fos = new FileOutputStream(convFile); 
 	    fos.write(multipart.getBytes());
 	    fos.close();
@@ -71,8 +74,46 @@ public class SpringBootController {
 	
 	
 	@PostMapping("/posts/{postId}/upload")
-	public void singleFileUpload(@PathVariable(value = "postId")Long postId, @RequestParam("file") MultipartFile multipart) throws IOException {
-		saveFileToDb(multipart, postId);
+	public String singleFileUpload(@PathVariable(value = "postId")Long postId, @RequestParam("file") MultipartFile multipart) throws IOException {
+		extension = FilenameUtils.getExtension(multipart.getOriginalFilename());
+		System.out.println(extension);
+		saveFileToDb(multipart, postId, extension);
+		return extension;
+	}
+	
+
+	@RequestMapping(value = "/getFile/{id}", method = RequestMethod.GET)
+	public String getFile(@PathVariable("id") String id) throws IOException {
+		byte[] image = null;
+		String str = "";
+		File folder = new File("C:\\Users\\Admin\\Documents\\JForum\\Forum-back-master\\images\\");
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+	    	str = file.getName().substring(0, file.getName().length()-4);
+	    	extension = file.getName().substring(file.getName().length()-3, file.getName().length());
+		    if (str.equals(id)) {
+	
+		    	return extension;
+		    }
+
+		    }
+		return "none";
+	}
+	
+
+	@RequestMapping(value = "/gif/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_GIF_VALUE)
+	public String getGif(@PathVariable("id") String id) throws IOException {
+		byte[] image = null;
+		File folder = new File("C:\\Users\\Admin\\Documents\\JForum\\Forum-back-master\\images\\");
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+		    if (file.getName().equals(id + ".gif")) {
+		    	byte[] fileContent = Files.readAllBytes(file.toPath());
+		    	String s = Base64.getEncoder().encodeToString(fileContent);
+		    	return s;
+		    }
+		    }
+		return "none";
 	}
 	
 	@RequestMapping(value = "/Image/{id}", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
@@ -89,7 +130,22 @@ public class SpringBootController {
 
 		    }
 		return "none";
+	}
+	
+	@RequestMapping(value = "/mp4/{id}", method = RequestMethod.GET)
+	public String getMp4(@PathVariable("id") String id) throws IOException {
+		byte[] image = null;
+		File folder = new File("C:\\Users\\Admin\\Documents\\JForum\\Forum-back-master\\images\\");
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+		    if (file.getName().equals(id + ".mp4")) {
+		    	byte[] fileContent = Files.readAllBytes(file.toPath());
+		    	String s = Base64.getEncoder().encodeToString(fileContent);
+		    	return s;
+		    }
 
+		    }
+		return "none";
 	}
 	//Method to create a person
 	@PostMapping("/person")
@@ -97,13 +153,29 @@ public class SpringBootController {
 		return personRepo.save(mSDM);
 	}
 	
-	//Get a person by ID
-	@GetMapping("/person/{id}")
-	public Users getPersonByID(@PathVariable(value = "id")Long personID) {
-		return personRepo.findById(personID).orElseThrow(()-> new ResourceNotFoundException("mySpringBootDataModel", "id", personID));
+	
+	//Method to create a person
+	@PostMapping("/person/login")
+	public String getPerson(@Valid @RequestBody Users mSDM) {
+		try {
+			System.out.println(mSDM.getUsername());
+			Users user = personRepo.findByusername(mSDM.getUsername());
+			if(user.getPassword().equals(mSDM.getPassword())) {
+				return "true";
+			}
+			else 
+			{
+				return "false";
+			}
+		}
+		catch (NullPointerException e) {
+			return "false";
+		}
 	}
 	
-	@RequestMapping("/person")
+
+	
+	@GetMapping("/person")
 	public List<Users> getAllUsers() {
 		return personRepo.findAll();
 	}
@@ -121,11 +193,22 @@ public class SpringBootController {
 		return postRepo.findAll();
 	}
 	
+	@SuppressWarnings("unlikely-arg-type")
 	@RequestMapping(value="/posts/{id}", method=RequestMethod.DELETE)
 	public Posts deletePerson(@PathVariable(value = "id")Long id) 
 	{
+		String str = "";
+		String Tid = id + "";
 		Posts pSDM = postRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("mySpringBootDataModel", "id", id));
 		postRepo.delete(pSDM);
+		File folder = new File("C:\\Users\\Admin\\Documents\\JForum\\Forum-back-master\\images\\");
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+	    	str = file.getName().substring(0, file.getName().length()-4);
+		    if (str.equals(Tid)) {
+		    	file.delete();
+		    }
+		}
 		return pSDM;
 	}
 	
@@ -197,18 +280,112 @@ public class SpringBootController {
 	}
 	
 	@RequestMapping(value="/posts/{postId}/comments/{commentId}", method=RequestMethod.DELETE)
-	public ResponseEntity<?> deleteComment(@PathVariable (value = "postId") long postId, @PathVariable (value = "commentId") long commentId)
+	public List<Comments> deleteComment(@PathVariable (value = "postId") long postId, @PathVariable (value = "commentId") long commentId)
 	{
 		if(!postRepo.existsById(postId)) {
 			throw new ResourceNotFoundException("Person", "id", postId);
 			}
 		return commentRepo.findById(commentId).map(comment -> {
 			commentRepo.delete(comment);
-			return ResponseEntitiy.ok().build();
+			List<Comments> allComs= commentRepo.findAll();
+			return allComs;
 		}).orElseThrow(()-> new ResourceNotFoundException("mySpringBootDataModel", "id", commentId));
+			
+			
 		}
 	
-	
+	@RequestMapping(value = "/sortPostsByUpvotes", method = RequestMethod.GET)
+	public List<Posts> sortAllPostsByUpvotes() {
+		List<Posts> posts = postRepo.findAll();
+		Posts[] postArr = (Posts[]) posts.toArray(new Posts[posts.size()]);
+        Boolean swapp;
+        int n = postArr.length-1;
+        do {
+            swapp = false;
+            for(int i = 0; i < n; i++){
+              if(postArr[i].getUpvotes() < postArr[i+1].getUpvotes()){
+            	  Posts temp = postArr[i];
+                  postArr[i] = postArr[i +1];
+                  postArr[i+1] = temp;
+                  swapp = true;
+              }
+            }
+            n--;
+        }while(swapp);
+        posts = Arrays.asList(postArr);
+		return posts;
 	}
+	
+	@RequestMapping(value = "/sortByDateAscending", method = RequestMethod.GET)
+	public List<Posts> sortByDateAscending(){
+		List<Posts> posts = postRepo.findAll();
+		Posts[] postArr = (Posts[]) posts.toArray(new Posts[posts.size()]);
+        Boolean swapp;
+        int n = postArr.length-1;
+        do {
+            swapp = false;
+            for(int i = 0; i < n; i++){
+              if(postArr[i].getDate().compareTo(postArr[i+1].getDate()) < 0){
+            	  Posts temp = postArr[i];
+                  postArr[i] = postArr[i +1];
+                  postArr[i+1] = temp;
+                  swapp = true;
+              }
+            }
+            n--;
+        }while(swapp);
+        posts = Arrays.asList(postArr);
+		return posts;
+	}
+	
+	
+	@RequestMapping(value = "/{postId}/sortByDateAscending", method = RequestMethod.GET)
+	public List<Comments> sortCommentsByDateAscending(@PathVariable (value = "postId")Long postId, Pageable pageable){
+		Page<Comments> comment = commentRepo.findByPostId(postId, pageable);
+		List<Comments> comments = comment.getContent();
+		Comments[] commentsArr = (Comments[]) comments.toArray(new Comments[comments.size()]);
+        Boolean swapp;
+        int n = commentsArr.length-1;
+        do {
+            swapp = false;
+            for(int i = 0; i < n; i++){
+              if(commentsArr[i].getCreatedDate().compareTo(commentsArr[i+1].getCreatedDate()) < 0){
+            	  Comments temp = commentsArr[i];
+                  commentsArr[i] = commentsArr[i +1];
+                  commentsArr[i+1] = temp;
+                  swapp = true;
+              }
+            }
+            n--;
+        }while(swapp);
+        comments = Arrays.asList(commentsArr);
+		return comments;
+	}
+	
+	@RequestMapping(value = "/{postId}/sortCommentsByUpvotes", method = RequestMethod.GET)
+	public List<Comments> sortAllCommentsByUpvotes(@PathVariable (value = "postId")Long postId, Pageable pageable) {
+		Page<Comments> comment = commentRepo.findByPostId(postId, pageable);
+		List<Comments> comments = comment.getContent();
+		Comments[] commentArr = (Comments[]) comments.toArray(new Comments[comments.size()]);
+        Boolean swapp;
+        int n = commentArr.length-1;
+        do {
+            swapp = false;
+            for(int i = 0; i < n; i++){
+              if(commentArr[i].getUpvotes() < commentArr[i+1].getUpvotes()){
+            	  Comments temp = commentArr[i];
+                  commentArr[i] = commentArr[i +1];
+                  commentArr[i+1] = temp;
+                  swapp = true;
+              }
+            }
+            n--;
+        }while(swapp);
+        comments = Arrays.asList(commentArr);
+		return comments;
+	}
+	
+	
+}
 	
 
--- src/main/java/com/qa/SweetSpot/repository/personRepository.java
+++ src/main/java/com/qa/SweetSpot/repository/personRepository.java
@@ -1,11 +1,15 @@
 package com.qa.SweetSpot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.qa.SweetSpot.mySpringBootDatabaseApp.*;
 
 @Repository
-public interface personRepository extends JpaRepository<Users,Long> {
-
+public interface personRepository extends JpaRepository<Users,String> {
+	
+	Users findByusername(@Param("username") String username);
 }
